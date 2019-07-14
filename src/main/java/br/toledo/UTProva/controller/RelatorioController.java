943863257258ad123c79b09de9655c05b2767e36
Dantas <fse_ddantas@uolinc.com>
2019-07-14 19:20:30 -0300

    Finalização do relatório simulado detalhado

--- src/main/java/br/toledo/UTProva/controller/RelatorioController.java
+++ src/main/java/br/toledo/UTProva/controller/RelatorioController.java
@@ -23,6 +23,7 @@ import com.itextpdf.text.Font;
 import com.itextpdf.text.FontFactory;
 import com.itextpdf.text.Image;
 import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
 import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.PdfPCell;
 import com.itextpdf.text.pdf.PdfPTable;
@@ -69,9 +70,9 @@ public class RelatorioController {
     @Autowired
     private ReportsDetalhadoRepository reportsDetalhadoRepository;
 
-    @PostMapping(value = "/01")
-    public void teste01(@RequestBody String channel) {
-        System.out.println("chanel  " + channel);
+    private static String UPLOAD_DIR = System.getProperty("user.dir");
+    public static String getUPLOAD_DIR() {
+        return UPLOAD_DIR;
     }
 
     @GetMapping(value = "/percentualDeAcerto/{idSimulado}/{tipo}")
@@ -143,183 +144,186 @@ public class RelatorioController {
     @GetMapping(value = "/relatorioDetalhadoPDF/{idSimulado}")
     public String reports2(@PathVariable("idSimulado") Long idSimulado)
             throws DocumentException, MalformedURLException, IOException {
-        List<DetalhadoReports> detalhadoReports = new ArrayList<>();
-
-        DetalhadoReports dReports = new DetalhadoReports();
-        dReports.setMatricula("1010");
-        dReports.setAluno("Teste nome Aluno 1");
-        dReports.setQtdAcertoFG(10);
-        dReports.setNotaObjetivasFG(75.9);
-        dReports.setQtdAcertoCE(5);
-        dReports.setNotaObjetivasCE(64.9);
-        dReports.setTotalAcertoObjetivas(15);
-        dReports.setNotaObjetivasTotal(87.6);
-        dReports.setNotaDiscursivaFG(30.7);
-        dReports.setNotaDiscursivaCE(35.9);
-        dReports.setNotaFinalFG(76.4);
-        dReports.setNotaFinalCE(43.6);
-        dReports.setNotaTotal(47.53);
-
-        DetalhadoReports dReports2 = new DetalhadoReports();
-        dReports2.setMatricula("2020");
-        dReports2.setAluno("Teste nome Aluno 2");
-        dReports2.setQtdAcertoFG(10);
-        dReports2.setNotaObjetivasFG(75.9);
-        dReports2.setQtdAcertoCE(5);
-        dReports2.setNotaObjetivasCE(64.9);
-        dReports2.setTotalAcertoObjetivas(15);
-        dReports2.setNotaObjetivasTotal(87.6);
-        dReports2.setNotaDiscursivaFG(30.7);
-        dReports2.setNotaDiscursivaCE(35.9);
-        dReports2.setNotaFinalFG(76.4);
-        dReports2.setNotaFinalCE(43.6);
-        dReports2.setNotaTotal(47.53);
-
-        detalhadoReports.add(dReports);
-        detalhadoReports.add(dReports2);
+       
         
-        String dest = "/Users/dantas/Projetos/detalhado.pdf";
-        Document document = new Document(PageSize.A4.rotate());
-        try {
-            PdfWriter.getInstance(document, new FileOutputStream(dest));
-            document.open();
-            PdfPTable table = new PdfPTable(14);
-            Image imgSoc = Image.getInstance("/Users/dantas/Projetos/logo_toledo.png");
-            imgSoc.scaleToFit(110,110);
-            // define font for table header row
-            Font font = FontFactory.getFont(FontFactory.COURIER, 8, BaseColor.BLACK); 
-            
-            PdfPCell cell = new PdfPCell(imgSoc, true);
-            cell.setBackgroundColor(BaseColor.ORANGE);
-            cell.setPadding(1);
-            cell.setBorder(1);
+        // Document document = new Document(PageSize.A4.rotate());
+     
+            String name = "Detalhado-" + LocalDateTime.now() + ".pdf";
+            File directory = new File(getUPLOAD_DIR() + "/reports/");
+
+            if (!directory.exists()) {
+                directory.mkdir();
+            }
+            String source = getUPLOAD_DIR() + "/reports/" + "" + name;
 
 
-            // write table header
-            cell.setPhrase(new Phrase("Matrícula", font));
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-            
-            cell.setPhrase(new Phrase("Nome do Aluno", font));
-            cell.setColspan(2);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
+            Image imgSoc = Image.getInstance(getUPLOAD_DIR() + "/imgs/unitoledo.png");
+            imgSoc.scaleToFit(100,100);
+
+            Document document = new Document(PageSize.A4.rotate());
+            try {
+                PdfWriter.getInstance(document, new FileOutputStream(source));
+                document.open();
+
+                Font f = new Font(Font.FontFamily.COURIER, 20, Font.BOLD);
+
+            Paragraph paragraph = new Paragraph("Relatório Detalhado | Simulado " + idSimulado, f);
+            paragraph.setAlignment(Element.ALIGN_CENTER);
+            paragraph.add(imgSoc);
+            paragraph.setSpacingAfter(20.0f);
+            document.add(paragraph);
+
+            PdfPTable table = new PdfPTable(13);
+
+            table.setWidthPercentage(100);
+            table.setHorizontalAlignment(0);
+            table.setSpacingAfter(10);
+                
+                // define font for table header row
+                Font font = FontFactory.getFont(FontFactory.COURIER, 6, BaseColor.BLACK); 
+                
+                PdfPCell cell = new PdfPCell(imgSoc, true);
+                cell.setBackgroundColor(BaseColor.ORANGE);
+                cell.setPadding(1);
+                cell.setBorder(1);
     
-            cell.setPhrase(new Phrase("Qtd de acertos em FG", font));
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-            
-            cell.setPhrase(new Phrase("Nota FG Objetivas(%)", font));
-            // cell.setColspan(2);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-            
-            cell.setPhrase(new Phrase("Qtd de acertos em CE", font));
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-            
-            cell.setPhrase(new Phrase("Nota CE Objetivas(%)", font));
-            // cell.setColspan(2);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell.setPhrase(new Phrase("Total de acertos objetivas", font));
-            // cell.setColspan(2);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell.setPhrase(new Phrase("Taxa de acertos total objetivas", font));
-            // cell.setColspan(2);
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell.setPhrase(new Phrase("Nota FG discursivas(%)", font));
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell.setPhrase(new Phrase("Nota CE discursivas(%)", font));
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell.setPhrase(new Phrase("Nota Final FG", font));
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell.setPhrase(new Phrase("Nota Final CE", font));
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            cell.setPhrase(new Phrase("Nota Final Total", font));
-            cell.setHorizontalAlignment(Element.ALIGN_CENTER);
-            table.addCell(cell);
-
-            Font font2 = FontFactory.getFont(FontFactory.COURIER, 8); 
-            
-            PdfPCell cell2 = new PdfPCell(imgSoc, true);
-            cell2.setPadding(2);
-            cell2.setBorder(1);
-            
-
-            reportsDetalhadoRepository.getDetalhado(idSimulado).forEach(n -> {
+    
+                // write table header
+                cell.setPhrase(new Phrase("Matrícula", font));
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
                 
+                cell.setPhrase(new Phrase("Nome do Aluno", font));
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
+        
+                cell.setPhrase(new Phrase("Qtd de acertos em FG", font));
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
                 
-                // cell2.setPhrase(new Phrase(n.getMatricula(), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
+                cell.setPhrase(new Phrase("Nota FG Objetivas(%)", font));
+                // cell.setColspan(2);
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
                 
-                // cell2.setPhrase(new Phrase(n.getAluno(), font2));
-                // cell2.setColspan(2);
-                // cell2.setHorizontalAlignment(Element.ALIGN_LEFT);
-                // table.addCell(cell2);
+                cell.setPhrase(new Phrase("Qtd de acertos em CE", font));
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
                 
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getQtdAcertoFG()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_LEFT);
-                // table.addCell(cell2);
-
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getNotaObjetivasFG()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
-
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getQtdAcertoCE()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
-
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getNotaObjetivasCE()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
-
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getTotalAcertoObjetivas()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
-
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getNotaObjetivasTotal()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
-
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getNotaDiscursivaFG()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
-
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getNotaDiscursivaCE()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
-
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getNotaFinalFG()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
-
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getNotaFinalCE()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
-
-                // cell2.setPhrase(new Phrase(String.valueOf(n.getNotaTotal()), font2));
-                // cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
-                // table.addCell(cell2);
-
-            });
-            
-            document.add(table);
-            document.close();
+                cell.setPhrase(new Phrase("Nota CE Objetivas(%)", font));
+                // cell.setColspan(2);
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
+    
+                cell.setPhrase(new Phrase("Total de acertos objetivas", font));
+                // cell.setColspan(2);
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
+    
+                cell.setPhrase(new Phrase("Taxa de acertos total objetivas", font));
+                // cell.setColspan(2);
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
+    
+                cell.setPhrase(new Phrase("Nota FG discursivas(%)", font));
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
+    
+                cell.setPhrase(new Phrase("Nota CE discursivas(%)", font));
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
+    
+                cell.setPhrase(new Phrase("Nota Final FG", font));
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
+    
+                cell.setPhrase(new Phrase("Nota Final CE", font));
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
+    
+                cell.setPhrase(new Phrase("Nota Final Total", font));
+                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+                table.addCell(cell);
+    
+                Font font2 = FontFactory.getFont(FontFactory.COURIER, 6); 
+                
+                PdfPCell cell2 = new PdfPCell(imgSoc, true);
+                cell2.setPadding(2);
+                cell2.setBorder(1);
+                
+    
+                reportsDetalhadoRepository.getDetalhado(idSimulado).forEach(n -> {
+                    
+                    
+                    cell2.setPhrase(new Phrase(n.getMatricula(), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+                    
+                    cell2.setPhrase(new Phrase(n.getAluno(), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_LEFT);
+                    table.addCell(cell2);
+                    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getQtdAcertoFG()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getNotaObjetivasFG()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getQtdAcertoCE()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getNotaObjetivasCE()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getTotalAcertoObjetivas()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getNotaObjetivasTotal()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getNotaDiscursivaFG()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getNotaDiscursivaCE()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getNotaFinalFG()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getNotaFinalCE()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                    cell2.setPhrase(new Phrase(String.valueOf(n.getNotaTotal()), font2));
+                    cell2.setHorizontalAlignment(Element.ALIGN_CENTER);
+                    table.addCell(cell2);
+    
+                });
+                
+                document.add(table);
+                document.close();
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
